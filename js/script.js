fbb87f2e26af23b71763d19caddae29a8b992c32
Dmitry <60636938+aj4kaa@users.noreply.github.com>
2020-12-15 05:42:37 +0300

    first practic in course

--- js/script.js
+++ js/script.js
@@ -1,6 +1,55 @@
-"use strict";
+/* Задание на урок:
 
-const isChecked = true;
-const isClose = false;
+1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
+'Сколько фильмов вы уже посмотрели?'
 
-console.log(isChecked && isClose);
+2) Создать объект personalMovieDB и в него поместить такие свойства:
+    - count - сюда передается ответ на первый вопрос
+    - movies - в это свойство поместить пустой объект
+    - actors - тоже поместить пустой объект
+    - genres - сюда поместить пустой массив
+    - privat - в это свойство поместить boolean(логическое) значение false
+
+3) Задайте пользователю по два раза вопросы:
+    - 'Один из последних просмотренных фильмов?'
+    - 'На сколько оцените его?'
+Ответы стоит поместить в отдельные переменные
+Записать ответы в объект movies в формате: 
+    movies: {
+        'logan': '8.1'
+    }
+
+Проверить, чтобы все работало без ошибок в консоли */
+
+'use strict';
+
+const numberOfFilms = +prompt('How many films you was whatch?', '');
+// console.log(numberOfFilms);
+
+let personalMovieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false
+};
+
+// personalMovieDB.count = numberOfFilms;
+
+// console.log(personalMovieDB);
+
+let film = prompt('Last was whatched movie?', ''),
+	ratig = prompt('How rating you give this film?', ''),
+	film2 = prompt('Last was whatched movie?', ''),
+	ratig2 = prompt('How rating you give this film?', '');
+
+/* let movies = {
+	[film]: ratig
+}; */
+// movies[film] = ratig;
+
+personalMovieDB.movies[film] = ratig;
+personalMovieDB.movies[film2] = ratig2;
+
+
+console.log(personalMovieDB);
