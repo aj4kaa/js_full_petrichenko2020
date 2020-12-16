ba63c0cb80ce235a0c5d7576545fcab5320952d9
Dmitry <60636938+aj4kaa@users.noreply.github.com>
2020-12-17 01:31:36 +0300

    home work 3

---
+++ js/script2.js
@@ -0,0 +1,67 @@
+'use strict';
+
+/* const str = 'hello world!';
+
+console.log(str.toLocaleUpperCase());
+console.log(str);
+
+console.log(str.indexOf('world'));
+
+const logg = 'Hello Dmitry!';
+
+console.log(logg.slice(6));
+console.log(logg.substring(6, 12));
+console.log(logg.substr(6, 6));
+ */
+
+const num = 12.2;
+console.log(Math.round(num));
+
+const test = '12.2px';
+console.log(parseInt(test));
+console.log(parseFloat(test));
+
+
+
+
+
+
+/* let num = 20;
+
+function showFirstMessage(text) {
+	console.log(text);
+	// let num =30;
+	console.log(num);
+}
+
+showFirstMessage('Hello World!');
+console.log(num);
+
+function calc(a, b) {
+	return (a * b);
+}
+
+console.log(calc(21, 30));
+
+function ret() {
+	let num = 50;
+
+	return num;
+}
+
+const anotherNum = ret();
+
+console.log(anotherNum); */
+
+/* const logger = function() {
+	console.log('qwesad');
+};
+
+logger();
+
+const calc = (a, b) => {
+	console.log('qweqwe');
+	return a + b;
+};
+
+console.log(calc(23, 44)); */
